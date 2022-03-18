636fab2df627021bfb94b859991bd8293f62eaee
igrek51 <igrek51.dev@gmail.com>
2022-03-18 16:05:22 +0100

    List dictionaries

--- backend/webdict/api/dictionary.py
+++ backend/webdict/api/dictionary.py
@@ -1,5 +1,27 @@
+from typing import Dict, Iterable, List
+
 from fastapi import FastAPI
+from asgiref.sync import sync_to_async
+
+from webdict.djangoapp.words import models
 
 
 def setup_dictionary_endpoints(app: FastAPI):
-    pass
+    @app.get("/dictionary")
+    async def dictionaries():
+        return await _list_dictionaries()
+
+
+@sync_to_async
+def _list_dictionaries() -> List[Dict]:
+    return list(_generate_dictionaries())
+
+
+def _generate_dictionaries() -> Iterable[Dict]:
+    objects = models.Dictionary.objects.all()
+    for object in objects:
+        yield {
+            "dictionaryId": object.id,
+            "sourceLanguage": object.source_language.code,
+            "targetLanguage": object.target_language.code,
+        }
--- backend/webdict/api/user.py
+++ backend/webdict/api/user.py
@@ -1,6 +1,6 @@
-from typing import Dict, Iterable
-from fastapi import FastAPI
+from typing import Dict, Iterable, List
 
+from fastapi import FastAPI
 from asgiref.sync import sync_to_async
 
 from webdict.djangoapp.words import models
@@ -8,19 +8,19 @@ from webdict.djangoapp.words import models
 
 def setup_user_endpoints(app: FastAPI):
     @app.get("/user")
-    async def info():
-        return [u async for u in _list_users()]
+    async def users():
+        return await _list_users()
 
 
-async def _list_users() -> Iterable[Dict]:
-    objects = await _get_all_users()
+@sync_to_async
+def _list_users() -> List[Dict]:
+    return list(_generate_users())
+
+
+def _generate_users() -> Iterable[Dict]:
+    objects = models.User.objects.all().order_by('login')
     for object in objects:
         yield {
             "id": object.id,
             "login": object.login,
         }
-
-
-@sync_to_async
-def _get_all_users():
-    return list(models.User.objects.all().order_by('login'))
