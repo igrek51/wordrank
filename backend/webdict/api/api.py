b937e1a37da6d3a256f3624c6740f9961a719f0b
igrek51 <igrek51.dev@gmail.com>
2022-03-18 15:20:47 +0100

    Serve api endpoints stub

--- backend/webdict/api/api.py
+++ backend/webdict/api/api.py
@@ -6,12 +6,27 @@ from fastapi.responses import RedirectResponse
 def creat_fastapi_app() -> FastAPI:
     app = FastAPI()
 
+    subapi = FastAPI()
+    setup_api_endpoints(subapi)
+    app.mount("/webdict/api", subapi)
+    app.mount("/webdict///api", subapi)
+
     @app.get("/")
     @app.get("/webdict")
     @app.get("/webdict/")
-    async def root():
-        return RedirectResponse('/webdict/index.html')
+    async def home():
+        return RedirectResponse("/webdict/index.html")
 
-    app.mount('/webdict', StaticFiles(directory="static"), name="webdict_static")
+    app.mount("/webdict", StaticFiles(directory="static"), name="webdict_static")
 
     return app
+
+
+def setup_api_endpoints(app: FastAPI):
+    @app.get("/info")
+    async def info():
+        return {
+            "environmentName": "WebDict",
+            "buildVersion": "2.0.1",
+            "status": "UP",
+        }
